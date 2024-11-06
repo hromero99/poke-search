f8e6813c7a73b391bd172692d13bc0f159825eb0
Héctor Romero López <hector@hromerol.es>
2024-11-06 16:18:25 +0100

    ADD: Button and input search done

---
+++ src/components/NavBar/navbarComponent.jsx
@@ -0,0 +1,16 @@
+
+import "./navbar.css"
+
+export const NavBarComponent = () => {
+
+
+    return <nav className="navbar">
+        <img src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"/>
+        <ul className="linkList">
+            <li>Pokemon</li>
+            <li>Ciudades</li>
+        </ul>
+
+    </nav>
+}
+
